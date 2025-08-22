4d49fa778277f4577fddb0cb9828c243f4e5e338
shadowxp <108752264+Sahil01010011@users.noreply.github.com>
2025-08-22 17:52:53 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,14 +5,14 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDGEpBoRUdVY7k8cdbvWiGhgKsDMNXE5aE",
-  authDomain: "anonchat-c6ac8.firebaseapp.com",
-  databaseURL: "https://anonchat-c6ac8-default-rtdb.firebaseio.com",
-  projectId: "anonchat-c6ac8",
-  storageBucket: "anonchat-c6ac8.firebasestorage.app",
-  messagingSenderId: "157788192653",
-  appId: "1:157788192653:web:5746c9bf6533c1845087c4",
-  measurementId: "G-ZDRF21G037"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
